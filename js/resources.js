b6168589a642aa20c9bc3a9c33d78882ea28d2f0
Mikael Nortikyan <nortikyan99@gmail.com>
2015-01-22 23:48:33 -0800

    Commented out code
    I commented out my code for the code that I wrote until video 5(Displaying Our Map)

--- js/resources.js
+++ js/resources.js
@@ -4,7 +4,9 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
+	 	// My Background Tiles
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+        // Meta-Tiles used for collisions
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
 	/* Atlases 
 	 * @example
@@ -17,6 +19,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
          * 
  	 */
+ 	 	// Maps/Levels that I make
         {name: "level01", type: "tmx", src: "data/map/test.tmx"},
 
 	/* Background music. 
--- js/screens/play.js
+++ js/screens/play.js
@@ -5,7 +5,7 @@ game.PlayScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 		// reset the score
 		game.data.score = 0;
-                
+                // Tells the Level Director to Load Level01
                 me.levelDirector.loadLevel("level01");
 
 		// add our HUD to the game world
