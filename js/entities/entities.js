a35dfa2198ef8e6131b8fa21aac333fed702c3e6
Mikael Nortikyan <nortikyan99@gmail.com>
2015-02-05 21:11:21 -0800

    Video 10
    I coded and commented out Video 10

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,46 +1,67 @@
-game.PlayerEntity = me.Entity.extend({
+game.PlayerEntity = me.Entity.extend ({
+	// this is a class
+	init: function(x, y, settings) {
+		// melon js uses this constructor on most things to help us set up
+		this._super(me.Entity, "init", [x, y, {
+			// this means reaching to the constructor of entites
+			image: "player",
+			width: 64,
+			// tells the screen what amount of space to reserve
+			height: 64,
+			spritewidth: "64",
+			// spritewidth/height are passing the main information tells what to do with the image
+			spriteheight: "64",
+			// these are setting the properties of the picture o the right demensions so the picture appears properly
+			getShape: function() {
+				return(new me.Rect(0, 0, 64, 64)).toPolygon();
+				// rect is what the guy can walk in to
+			}
+		}]);
 
-	init: function(x, y, settings){
-			// 
-			this._super(me.Entity,  'init', [x, y, {
-				image: "player",
-				// Image has a width of 64
-				width: 64,
-				// Images has a height of 64
-				height:64,
-				// These Two are passing the main information
-				spritewidth:"64",
-				spriteheight:"64",
-				// Returns a new shape, a rectangle. What the little guy could walk into.
-				// Turned it into a polygon with .toPolygon
-				getShape: function(){
-					return(new me.Rect(0, 0, 64, 64)) .toPolygon();
-				}
-			}]);
+		this.body.setVelocity(5, 20);
+		// this a lso changes the y velocity of the character
+		// this is the movement speed of the character
 
-			// Player's location
-			this.body.setVelocity(5, 20);
+		this.renderable.addAnimation("idle", [78]);
+		// when the character is still this is what he will look like
+		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+		// this is going to be what the cahracter is going to change into
 
-			this.renderable.addAnimation("idle", [78]);
-			this.renderable.addAnimation("walk", [117,118,119,120,121,123,124,125],80);
+		this.renderable.setCurrentAnimation("idle");
+	}, 
 
-			
-	},
-
-	// Delta is the change in time that's happened
-	update: function(delta){
-		if(me.input.isKeyPressed("right")){
-			// adds to the position of my x by the velocity defined above in me.time.tick
+	update: function(delta) {
+		// this function is what happens on the fly
+		if(me.input.isKeyPressed("right")) {
+			// set the position of my x by adding the velocity to find above in set veloctiy 
+			// setVeloctiy() and multiplying it by timer.tick
 			// me.timer.tick makes the movement look smooth
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
-		}else{
+			this.flipX("true");
+			// this is flipping the animation around
+
+		}
+		else {
 			this.body.vel.x = 0;
 		}
-		
 
-		// Tells it to all work
+		if(this.body.vel.x !== 0) 
+		//allowing the guy to not immediately walk 
+		{
+			if(!this.renderable.isCurrentAnimation("walk")) {
+				this.renderable.setCurrentAnimation("walk");
+				// this says it doesnt want to start the walk animation if it is already walking
+			}
+		}
+		else {
+			this.renderable.setCurrentAnimation("idle");
+		}
+		// delta is the change in time that has happens
+
 		this.body.update(delta);
-		return true;
 
+		this._super(me.Entity, "update", [delta]);
+		// this is updating the animations on the fly
+		return true;
 	}
 });
