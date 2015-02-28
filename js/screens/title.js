b9ec8ad517e0a26599478602d47f1462948e9bdd
Mikael Nortikyan <nortikyan99@gmail.com>
2015-02-27 20:16:51 -0800

    Video 27
    I coded and commented Video 27 out about Making the Title Screen Functional

--- js/screens/title.js
+++ js/screens/title.js
@@ -3,7 +3,31 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		; // TODO
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); 
+		// this is showing the title screen
+		me.input.bindKey(me.input.KEY.ENTER, "start");
+
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function() {
+				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height])
+				// making a call to the super function
+				this.font = new me.Font("Arial", 46, "white");
+				// setting a font on the screen
+			}, 
+
+            draw: function(renderer) {
+		this.font.draw(renderer.getContext(), "Awesomenauts", 450, 130);
+		// when we are drawing something we are passing in the context of where we are
+		this.font.draw(renderer.getContext(), "Press ENTER to play", 250, 530);
+		}         })));
+
+			this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+			// this is listening for someone pressing the enter button
+			if (action === "start") {
+				me.state.change(me.state.PLAY);
+				}
+			});
+			
 	},
 	
 	
@@ -11,6 +35,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+		me.input.unbindKEY(me.input.KEY.ENTER); // TODO
+		me.event.unsubscribe(this.handler);
 	}
 });
