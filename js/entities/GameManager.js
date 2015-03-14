7f77ec08e784b4eef73c446074dba89b89c96a99
Mikael Nortikyan <nortikyan99@gmail.com>
2015-03-13 20:28:18 -0700

    Video 47
    Coded and commented video 47

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -88,7 +88,7 @@ game.ExperienceManager = Object.extend ({
 		this.gameover = true;
 		me.save.exp = game.data.exp;
 		// for testing code purposes
-		console.log("Juzet is awesome " + me.save.exp);
+		console.log("Barev Aper" + me.save.exp);
 		me.save.exp2 = 4;
 	}
 });
@@ -98,10 +98,38 @@ game.SpendGold = Object.extend ({
 		this.lastBuy = new Date().getTime();
 		this.paused = false;
 		this.alwaysUpdate = true;	
+		this.updateWhenPaused = true;
+		this.buying = false;
 	},
 
 	update: function() {
+		this.now = new Date().getTime();
+		if (me.input.isKeyPressed("buy") && this.now-this.lastBuy >= 1000 ) {
+			this.lastBuy = this.now;
+			if (!this.buying) {
+				this.startBuying();
+			}
+			else {
+				this.stopBuying();
+			}
+
+		}
 		return true;
 	}, 
-
+	startBuying: function () {
+		this.buying = true;
+		me.state.pause(me.state.PLAY);
+		game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+		game.data.buyScreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
+		game.data.buyScreen.updateWhenPaused = true;
+		game.data.buyScreen.setOpacity(0.8);
+		me.game.world.addChild(game.data.buyscreen, 34);
+		game.data.player.body.setVelocity(0, 0);
+	},
+	stopBuying: function () {
+		this.buying = false;
+		me.state.resume(me.state.PLAY);
+		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+		me.game.world.removeChild(game.data.buyscreen);
+	}
 });
