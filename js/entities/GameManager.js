af666b10b2623fac952eb192c500552458f9dbff
Mikael Nortikyan <nortikyan99@gmail.com>
2015-03-30 08:19:08 -0700

    Video 50
    Coded and commented video 50

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -116,6 +116,8 @@ game.SpendGold = Object.extend ({
 			}
 
 		}
+		this.checkBuyKeys();
+
 		return true;
 	}, 
 	startBuying: function () {
@@ -153,6 +155,12 @@ game.SpendGold = Object.extend ({
             draw: function(renderer) {
 				this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT", this.pos.x, this.pos.y);
 				// when we are drawing something we are passing in the context of where we are
+				this.font.draw(renderer.getContext(), "Skill 1: Increase Damage. Current Level: " + game.data.skill1 + " Cost: " + ((game.data.skill1+1)*10), this.pos.x, this.pos.y + 40);
+				this.font.draw(renderer.getContext(), "Skill 2: Run faster. Current Level: " + game.data.skill2 + " Cost: " + ((game.data.skill2+1)*10), this.pos.x, this.pos.y + 80);
+				this.font.draw(renderer.getContext(), "Skill 3: Increase Health. Current Level: " + game.data.skill3 + " Cost: " + ((game.data.skill3+1)*10), this.pos.x, this.pos.y + 120);
+				this.font.draw(renderer.getContext(), "Q Ability: Speed Burst. Current Level: " + game.data.ability1 + " Cost: " + ((game.data.ability1+1)*10), this.pos.x, this.pos.y + 160);
+				this.font.draw(renderer.getContext(), "W Ability: Eat Creep for Health. Current Level: " + game.data.ability2 + " Cost: " + ((game.data.ability2+1)*10), this.pos.x, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "E Ability: Throw your spear. Current Level: " + game.data.ability3+ " Cost: " + ((game.data.ability3+1)*10), this.pos.x, this.pos.y + 240);
 			}   
 
 		}));
@@ -174,5 +182,96 @@ game.SpendGold = Object.extend ({
 		me.game.world.removeChild(game.data.buytext);
 		// these are stopping the functions
 		// this is unbinding the functions for when you are not buying
+	},
+
+	checkBuyKeys: function() {
+		if (me.input.isKeyPressed("F1")) {
+			if(this.checkCost(1)){
+				this.makePurchase(1);
+			}
+		}
+		else if (me.input.isKeyPressed("F2")) {
+			if(this.checkCost(2)){
+				this.makePurchase(2);
+			}
+		}
+		else if (me.input.isKeyPressed("F3")) {
+			if(this.checkCost(3)){
+				this.makePurchase(3);
+			}
+		}
+		else if (me.input.isKeyPressed("F4")) {
+			if(this.checkCost(4)){
+				this.makePurchase(4);
+			}
+		}
+		else if (me.input.isKeyPressed("F5")) {
+			if(this.checkCost(5)){
+				this.makePurchase(5);
+			}
+		}
+		else if (me.input.isKeyPressed("F6")) {
+			if(this.checkCost(6)){
+				this.makePurchase(6);
+			}
+		}
+	}, 
+	
+	checkCost: function(skill) {
+		if(skill===1 && (game.data.gold >= ((game.data.skill1 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else if(skill===2 && (game.data.gold >= ((game.data.skill2 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 0 for the cost
+			return true;
+		}
+		else if(skill===3 && (game.data.gold >= ((game.data.skill3 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else if(skill===4 && (game.data.gold >= ((game.data.ability1 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else if(skill===5 && (game.data.gold >= ((game.data.ability2 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else if(skill===6 && (game.data.gold >= ((game.data.ability3 + 1)*10))) {
+			// taking the skill level adding 1 and multiplying it by 10 for the cost
+			return true;
+		}
+		else {
+			return false;
+		}
+	},
+	
+	makePurchase: function(skill) {
+		if(skill===1) {
+			game.data.gold -= ((game.data.skill1 +1)*10);
+			game.data.skill1 += 1;
+			game.data.player.attack += 1; 
+		}
+		else if(skill===2) {
+			game.data.gold -= ((game.data.skill2 +1)*10);
+			game.data.skill2 += 1;
+		}
+		else if(skill===3) {
+			game.data.gold -= ((game.data.skill3 +1)*10);
+			game.data.skill3 += 1;
+		}
+		if(skill===4) {
+			game.data.gold -= ((game.data.ability1 +1)*10);
+			game.data.ability1 += 1;
+		}
+		if(skill===5) {
+			game.data.gold -= ((game.data.ability2 +1)*10);
+			game.data.ability2 += 1;
+		}
+		if(skill===6) {
+			game.data.gold -= ((game.data.ability3 +1)*10);
+			game.data.ability3 += 1;
+		}
 	}
 });
