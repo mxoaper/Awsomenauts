04b2b38cb762e7acbefddab5edf6d21bc0bb3df1
Mikael Nortikyan <nortikyan99@gmail.com>
2015-04-05 23:58:51 -0700

    Comments

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -28,3 +28,74 @@
 
 
     });
+
+
+
+
+// The game manager removes the player if he's  dead and sets him back
+
+// we have refactored the game manager as much as possible
+
+// There are some other things that we can also refactor
+
+// this.gameOver is a quick fix for all the madness that is going on 
+
+//this.game over is the global variable we are using to call the game over screen
+
+//We only get into each of those if statements if game over is false
+
+
+// if it is true we can not add any more points
+
+
+// we need to apply this.gameOver to something that will call the game over clean
+
+//we reduced lines of code that were repetitive 
+
+// A bollean means either true or false and it is a variable type
+
+// we are automatically calling the game over function with 2 intenitions
+
+//what we basically did was we set the values up for when a user purchases a power up and we set up the cost of the experience
+
+// the user can now use the experience he/she has and buy powerups so far we only have 1 power up that is available to be purchased
+
+//Basically what we did in video 46 was we set up the spendGold function
+
+// we also added a picture of it in the title screen
+
+// we linked everything together that had to do with the spendGold funciton
+
+
+//When the user presses the buy button we want to pause the game
+
+//we are setting up the function to where the buy key is pressed
+
+// resume is what we use as the pause function in stopBuying
+
+//the opacity is adjustable to the user can still get a glimpse of what is going on in the game at the current time
+
+// The reson why we unbind the key is so people can not randomly make purchases
+
+// this.updateWhenPaused will only work if it is after this.font
+
+// we had to add a z value for our "B" button to wrk
+
+// we had to add the z value for the text to show up
+
+// we are adding new upgrades to be able o be purchased for upgrading your player
+
+// We are setting up the names and buttons of these upgrades
+
+// we also added the cost and the current level for each of these upgrades or powerups
+
+// now we are going to set up our buttons to specific functions
+
+// checkBuyKeys only checks the key presses
+
+// we pass a prameter on checkCost so it is universal and it works on all skills 
+
+
+// if we do not have enough we will return false
+
+// your skill will have one added to it and multiply by 10
--- php/controller/login-user.php
+++ php/controller/login-user.php
@@ -8,40 +8,35 @@
 		'exp3' => '',
 		'exp4' => '',
      );
-
-
 	// this gives us access to our data base
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 	// we put input post because our method is from post
 	$password = filter_input (INPUT_POST, "password", FILTER_SANITIZE_STRING);
+	$
 	$query = $_SESSION["connection"]->query("SELECT * FROM users WHERE BINARY username = '$username'");
 	// we are selecting our salt and our password from our users table where our username is the username that was sent in via the post
 	if($query->num_rows == 1) {
 		//num rows checks wether or not the rows are equal to 1	
 		$row = $query->fetch_array();
 		// we have to fetch the array stored in the quary var
-
-		if($row["password"] === crypt($password, $row["salt"])) {
+		if($row["password"] === crypt ($password, $row["salt"])) {
       		$_SESSION["authenticated"] = true;
       		$array["exp"] = $row["exp"];
       		$array["exp1"] = $row["exp1"];
       		$array["exp2"] = $row["exp2"];
       		$array["exp3"] = $row["exp3"];
       		$array["exp4"] = $row["exp4"];
-    		$_SESSION["name"] = $username;
       		echo json_encode($array);
-		}else {
-			echo "Invalid username and password";
 		}
-	}else {
-		echo "Invalid username and password";
+		else {
+			echo "<p>Invalid username and password</p>";
+		}
+	}
+	else {
+		echo "<p> Invalid username and password</p>";
 	}
 // we are editing our php code from the blog
-
 // we are building an array in login-user.php
-
 // an array is a series of objects
-
 // $row is our users experience
-
 // json lets us echo an array as a statement
