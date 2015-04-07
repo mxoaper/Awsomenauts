dbe78cba710eb353da71adbd0a739b89c0460b05
Mikael Nortikyan <nortikyan99@gmail.com>
2015-04-06 22:19:53 -0700

    Video i1

--- js/game.js
+++ js/game.js
@@ -4,22 +4,23 @@ var game = {
 	// an object where to store game information
 	data : {
 		// score
-		score : 0,
+		score : 0, 
 		enemyBaseHealth: 1,
-		playerBaseHealth: 1,
-		enemyCreepHealth: 1,
-		playerHealth: 1,
+		playerBaseHealth: 5,
+		enemyCreepHealth: 4,
+		playerHealth: 15,
+
 		enemyCreepAttack: 1,
 		playerAttack: 1,
 		playerAttackTimer: 1000,
-		enemyCreepAttackTimer: 1000,
+		enemycreepAttackTimer: 1000,
 		playerMoveSpeed: 5,
 		creepMoveSpeed: 5,
-		gameManager: "",
-		HeroDeathManager: "",
+		gameTimerManager: "",
+		gameHeroDeathManager: "",
+		spearTimer: 15,
 		player: "",
 		exp: 0,
-		// these are going to be used to buy stuff
 		gold: 0,
 		ability1: 0,
 		ability2: 0,
@@ -33,46 +34,44 @@ var game = {
 		exp4: 0,
 		win: "",
 		pausePos: "",
-		buyScreen: "",
-		buyText: ""
-		// variable that will affeect the movement etc of the character
+		buyscreen: "",
+		buytext: ""
+
+
 	},
 	
 	
 	// Run on page load.
 	"onload" : function () {
-	// Initialize the video.
-	if (!me.video.init("screen",  me.video.CANVAS, 1076, 600, true, '1.0')) {
-		// we changed the numbers after canvas because we want the game to fit our screen
-		// this is the basic set up of the game appearance
-		alert("Your browser does not support HTML5 canvas.");
-		return;
-	}
+		// Initialize the video.
+		if (!me.video.init("screen",  me.video.CANVAS, 1067, 600, true, '1.0')) {
+			alert("Your browser does not support HTML5 canvas.");
+			return;
+		}
 
-	// add "#debug" to the URL to enable the debug Panel
-	if (document.location.hash === "#debug") {
-		window.onReady(function () {
-			me.plugin.register.defer(this, debugPanel, "debug");
-		});
-	}
-	// me.save.add({exp: 0, exp1: 0,exp: 2, exp3: 0, exp4: 0});
+		// add "#debug" to the URL to enable the debug Panel
+		if (document.location.hash === "#debug") {
+			window.onReady(function () {
+				me.plugin.register.defer(this, debugPanel, "debug");
+			});
+		}
+		
+		me.state.SPENDEXP = 112;
+		me.state.LOAD = 113;
+		me.state.NEW = 114;
 
-	me.state.SPENDEXP = 112;
-	me.state.LOAD = 113;
-	me.state.NEW = 114;
+		// Initialize the audio.
+		me.audio.init("mp3.episode,ogg");
 
-	// Initialize the audio.
-	me.audio.init("mp3,ogg");
+		// Set a callback to run when loading is complete.
+		me.loader.onload = this.loaded.bind(this);
 
-	// Set a callback to run when loading is complete.
-	me.loader.onload = this.loaded.bind(this);
+		// Load the resources.
+		me.loader.preload(game.resources);
 
-	// Load the resources.
-	me.loader.preload(game.resources);
-
-	// Initialize melonJS and display a loading screen.
-	me.state.change(me.state.LOADING);
-},
+		// Initialize melonJS and display a loading screen.
+		me.state.change(me.state.LOADING);
+	},
 
 	// Run on game resources loaded.
 	"loaded" : function () {
@@ -80,23 +79,25 @@ var game = {
 		me.pool.register("PlayerBase", game.PlayerBaseEntity);
 		me.pool.register("EnemyBase", game.EnemyBaseEntity);
 		me.pool.register("EnemyCreep", game.EnemyCreep, true);
-		me.pool.register("FriendCreep", game.FriendCreep,true);
-		me.pool.register("HeroCreep", game.HeroCreep,true);
 		me.pool.register("GameTimerManager", game.GameTimerManager);
 		me.pool.register("HeroDeathManager", game.HeroDeathManager);
-		me.pool.register("ExperienceManager", game.ExperienceManager);
-		me.pool.register("SpendGold", game.SpendGold);
-		// registering the enemy and player base to the pool
-		// the character is being added and connected to the entites file
-		// true says any object you register with true you can make multiple instances of
+	    me.pool.register("ExperienceManager", game.ExperienceManager);
+	    me.pool.register("SpendGold", game.SpendGold);
+	    me.pool.register("spear", game.SpearThrow);
+
+		// this is adding our player to the pool of objects we can use
+        // we are connecting our player to the player entity class we just built.
+        // true allows you to make multiple instances of that object
 
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
-		me.state.set(me.state.SPENDEXP, new game.spendExp());
+		me.state.set(me.state.SPENDEXP, new game.SpendExp());
 		me.state.set(me.state.LOAD, new game.LoadProfile());
 		me.state.set(me.state.NEW, new game.NewProfile());
+
 
 		// Start the game.
 		me.state.change(me.state.MENU);
 	}
 };
+// we dont need to set the player base to true because there is only one
--- js/resources.js
+++ js/resources.js
@@ -2,23 +2,21 @@ game.resources = [
 
 	/* Graphics. 
 	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-	{name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
-	// we are just loading the background tiles to the template 
-	{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
-	// we are also loading the meta tiles to the template
-	{name: "player", type:"image", src: "data/img/OrcSpear.png"},
-	// we are loading the player image to the template
-	{name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
-	{name: "creep1", type:"image", src: "data/img/brainmonster.png"},
-	{name: "creep2", type:"image", src: "data/img/gloop.png"},
-	{name: "creep3", type:"image", src: "data/img/elf.png"},
-	{name: "title-screen", type:"image", src: "data/img/title.png"},
-	{name: "exp-screen", type:"image", src: "data/img/loadpic.png"},
-	{name: "gold-screen", type:"image", src: "data/img/spend.png"},
-	{name: "load-screen", type:"image", src: "data/img/loadpic.png"},
-	{name: "new-screen", type:"image", src: "data/img/newpic.png"},
+	 {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	 {name: "wall", type:"image", src: "data/img/wall.jpg"},
+     {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+     {name: "player", type:"image", src: "data/img/orcSpear.png"},
+     {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
+     {name: "creep1", type:"image", src: "data/img/brainmonster.png"},
+     {name: "title-screen", type:"image", src: "data/img/title.png"},
+     {name: "exp-screen", type:"image", src: "data/img/loadpic.png"},
+     {name: "gold-screen", type:"image", src: "data/img/spend.png"},
+     {name: "load-screen", type:"image", src: "data/img/loadpic.png"},
+     {name: "new-screen", type:"image", src: "data/img/newpic.png"},
+     {name: "spear", type:"image", src: "data/img/spear.png"},
+      // this line above, is where we are loading the image or "player" that we are using
+
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -26,24 +24,21 @@ game.resources = [
 		
 	/* Maps. 
 	 * @example
-	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
- 	{name: "level01", type: "tmx", src: "data/map/test.tmx"},
- 	// this is displaying our full background images
+ 	  {name: "level01", type: "tmx", src: "data/map/test.tmx"},
+     // we use this one because it is under the tmx and that is what we used
+
 	/* Background music. 
+
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
 	 */	
-	// {name: "what", type: "audio", src: "data/bgm/"},
-	// {name: "blankSpace", type: "audio", src: "data/bgm/"},
-	// {name: "katy1", type: "audio", src: "data/bgm/"},
-	// {name: "miley", type: "audio", src: "data/bgm/"},
 	 
+
+
 	/* Sound effects. 
 	 * @example
 	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
 	 */
-	{name: "jump", type: "audio", src: "data/sfx/"},
-
 ];
