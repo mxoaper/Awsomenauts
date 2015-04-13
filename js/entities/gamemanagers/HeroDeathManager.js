309222bbf4d4b5a91d364af026ee32f397e57c34
Mikael Nortikyan <nortikyan99@gmail.com>
2015-04-13 01:41:06 -0700

    Video i4

---
+++ js/entities/gamemanagers/HeroDeathManager.js
@@ -0,0 +1,20 @@
+    game.HeroDeathManager = Object.extend({
+      init: function(x, y, settings){
+      this.alwaysUpdate = true;
+      this.gameOver = false;
+
+    },
+
+
+  update: function(){
+    if(game.data.player.dead) {
+       me.game.world.removeChild(game.data.player);
+       me.game.world.removeChild(game.data.miniPlayer);
+       me.state.current().resetPlayer(10, 0);
+
+      }
+
+      return true;
+  }
+
+});
